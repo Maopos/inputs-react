c22e896356c1abde2c9d966106b6ade9f177d2a0
maopos <mauricioposadamurillo@gmail.com>
2022-02-12 19:45:38 -0500

    Tipos de inputs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="">
+        <h2>Tipos de Inputs</h2>
+          <label htmlFor="">
+            Text: <input type="text" />
+          </label>
+          <label htmlFor="">
+            Search: <input type="search" />
+          </label>
+          <label htmlFor="">
+            Number: <input type="number" />
+          </label>
+          <label htmlFor="">
+            Tel: <input type="tel" />
+          </label>
+          <label htmlFor="">
+            Password: <input type="password" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Date: <input type="date" />
+          </label>
+          <label htmlFor="">
+            DateTimeLocal: <input type="datetime-local" />
+          </label>
+          <label htmlFor="">
+            Month: <input type="month" />
+          </label>
+          <label htmlFor="">
+            Week: <input type="week" />
+          </label>
+          <label htmlFor="">
+            Time: <input type="time" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Email: <input type="email" />
+          </label>
+          <label htmlFor="">
+            Url: <input type="url" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Color: <input type="color" />
+          </label>
+          <label htmlFor="">
+            File: <input type="file" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Checkbox: <input type="checkbox" />
+          </label>
+          <label htmlFor="">
+            Radio: <input type="radio" />
+          </label>
+          <label htmlFor="">
+            Range: <input type="range" />
+          </label>
+          <br />
+          <label htmlFor="">
+            Reset: <input type="reset" />
+          </label>
+          <label htmlFor="">
+            Submit: <input type="submit" />
+          </label>
+        </form>
       </header>
     </div>
   );
